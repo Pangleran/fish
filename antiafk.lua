066c6ce7f7c3746de5fb23ddd9d12334b788c1a8
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-23 08:11:45 +0000

    auto commit Sun Nov 23 08:11:45 AM UTC 2025

--- antiafk.lua
+++ antiafk.lua
@@ -1,5 +1,16 @@
 local AntiAfk = {}
 
 function AntiAfk.Apply()
-
+  if state then
+    AntiAFKConnection = LocalPlayer.Idled:Connect(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
+  else
+    if AntiAFKConnection then
+        AntiAFKConnection:Disconnect()
+        AntiAFKConnection = nil
+    end
+  end
+  
 return AntiAfk
