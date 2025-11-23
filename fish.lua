7129729cb5fd996fa9a9478de5b0df7337743425
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-23 08:57:03 +0000

    auto commit Sun Nov 23 08:57:03 AM UTC 2025

--- fish.lua
+++ fish.lua
@@ -50,7 +50,7 @@ Fishing:CreateToggle({
     CurrentValue = false,
     Flag = "AutoFishingToggle",
     Callback = function(state)
-        AutoFish.Apply
+        AutoFish.Apply(state)
     end,
 })
 
