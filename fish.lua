ad353c924d50c1477c2c069e3b02cf621846efc6
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-23 15:27:57 +0800

    Create fish.lua

---
+++ fish.lua
@@ -0,0 +1,89 @@
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Fish it - Iky Fareza",
+    ConfigurationSaving = {
+        Enabled = false
+    }
+})
+
+local VirtualUser = game:GetService("VirtualUser")
+local LocalPlayer = game:GetService("Players").LocalPlayer
+
+local Teleport = Window:CreateTab("Teleport")
+
+Teleport:CreateButton({
+    Name = "Teleport to Ocean",
+    Callback = function()
+        local player = game.Players.LocalPlayer
+        local char = player.Character or player.CharacterAdded:Wait()
+        local root = char:WaitForChild("HumanoidRootPart")
+        root.CFrame = CFrame.new(-2659.45972, 5.53963947, -4.4157052)
+    end,
+})
+
+Teleport:CreateButton({
+    Name = "Teleport to Creater",
+    Callback = function()
+        local player = game.Players.LocalPlayer
+        local char = player.Character or player.CharacterAdded:Wait()
+        local root = char:WaitForChild("HumanoidRootPart")
+    end,
+})
+
+local Fishing = Window:CreateTab("Fishing")
+
+Fishing:CreateToggle({
+    Name = "Auto Fishing",
+    CurrentValue = false,
+    Flag = "AutoFishingToggle",
+    Callback = function(state)
+        -- code
+    end,
+})
+
+local Settings = Window:CreateTab("Settings")
+
+Settings:CreateToggle({
+    Name = "Anti AFK",
+    CurrentValue = false,
+    Flag = "AntiAFKToggle",
+    Callback = function(state)
+        if state then
+            AntiAFKConnection = LocalPlayer.Idled:Connect(function()
+                VirtualUser:CaptureController()
+                VirtualUser:ClickButton2(Vector2.new())
+            end)
+        else
+            if AntiAFKConnection then
+                AntiAFKConnection:Disconnect()
+                AntiAFKConnection = nil
+            end
+        end
+    end,
+})
+
+Settings:CreateButton({
+    Name = "Low Texture",
+    Callback = function()
+        for _, v in pairs(game:GetDescendants()) do
+            if v:IsA("BasePart") then
+                v.Material = Enum.Material.SmoothPlastic
+                v.Reflectance = 0
+            elseif v:IsA("Decal") or v:IsA("Texture") then
+                v.Transparency = 1
+            end
+        end
+
+        local Lighting = game:GetService("Lighting")
+        for _, effect in pairs(Lighting:GetChildren()) do
+            if effect:IsA("PostEffect") then
+                effect.Enabled = false
+            end
+        end
+
+        Lighting.GlobalShadows = false
+        Lighting.FogEnd = 1e10
+        settings().Rendering.QualityLevel = "Level01"
+    end,
+})
