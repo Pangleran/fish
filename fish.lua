56d273a0bfb8cb366aff8afb41bab1ab95880a25
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-23 08:11:35 +0000

    auto commit Sun Nov 23 08:11:35 AM UTC 2025

--- fish.lua
+++ fish.lua
@@ -47,17 +47,7 @@ Settings:CreateToggle({
     CurrentValue = false,
     Flag = "AntiAFKToggle",
     Callback = function(state)
-        if state then
-            AntiAFKConnection = LocalPlayer.Idled:Connect(function()
-                VirtualUser:CaptureController()
-                VirtualUser:ClickButton2(Vector2.new())
-            end)
-        else
-            if AntiAFKConnection then
-                AntiAFKConnection:Disconnect()
-                AntiAFKConnection = nil
-            end
-        end
+        
     end,
 })
 
