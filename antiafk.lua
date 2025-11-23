6f9b9703b154c277493b57c7504bc700a6c09c7b
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-23 08:37:07 +0000

    auto commit Sun Nov 23 08:37:07 AM UTC 2025

--- antiafk.lua
+++ antiafk.lua
@@ -1,9 +1,15 @@
 local AntiAfk = {}
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait()
 local VirtualUser = game:GetService("VirtualUser")
-local LocalPlayer = game:GetService("Players").LocalPlayer
+
+local AntiAFKConnection
 
 function AntiAfk.Apply(state)
   if state then
+    if AntiAFKConnection then AntiAFKConnection:Disconnect() end
+
     AntiAFKConnection = LocalPlayer.Idled:Connect(function()
       VirtualUser:CaptureController()
       VirtualUser:ClickButton2(Vector2.new())
