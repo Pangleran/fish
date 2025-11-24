a87acf92be27f15e69522d45fd925db88b2e32f2
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-24 01:08:28 +0000

    auto commit Mon Nov 24 01:08:28 AM UTC 2025

--- teleport.lua
+++ teleport.lua
@@ -11,7 +11,7 @@ function TeleportModule.ToClassicIsland()
     local player = game.Players.LocalPlayer
     local char = player.Character or player.CharacterAdded:Wait()
     local root = char:WaitForChild("HumanoidRootPart")
-    root.CFrame = CFrame.new(1254.07678, 9.99999905, 5093.01611)
+    root.CFrame = CFrame.new(1254.07678, 9.99999905, 28.23)
 end
 
 function TeleportModule.ToUnderground()
