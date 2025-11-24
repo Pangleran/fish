e336263c3169e4d4dc29de011345c7cf22d1a9c9
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-24 02:16:46 +0000

    auto commit Mon Nov 24 02:16:46 AM UTC 2025

--- teleport.lua
+++ teleport.lua
@@ -17,7 +17,8 @@ end
 function TeleportModule.ToIronCavern()
     local player = game.Players.LocalPlayer
     local char = player.Character or player.CharacterAdded:Wait()
-    local root = char:WaitForChild("")
+    local root = char:WaitForChild("HumanoidRootPart")
+    root.
 
 function TeleportModule.ToUnderground()
     local player = game.Players.LocalPlayer
