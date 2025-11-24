7da1ad60a5d8af2f8e90b8577b8a95483ca15b92
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-24 12:49:32 +0000

    auto commit Mon Nov 24 12:49:32 PM UTC 2025

--- fish.lua
+++ fish.lua
@@ -18,7 +18,7 @@ task.spawn(function()
     if core then
         local toggle = core:WaitForChild("Toggle", 10)
         if toggle and toggle:FindFirstChild("TextLabel") then
-            toggle.TextLabel.Text = "Buka Menu" -- ubah sesuai keinginan
+            toggle.TextLabel.Text = "Sc"
         end
     end
 end)
