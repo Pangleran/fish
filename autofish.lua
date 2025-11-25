92dfae9acdaf474ff67375541b55eb728a90a1a5
Pangleran <mukhammadresky24@gmail.com>
2025-11-25 03:52:15 +0000

    auto commit Tue Nov 25 03:52:15 AM UTC 2025

--- autofish.lua
+++ autofish.lua
@@ -16,7 +16,11 @@ local Events = {
 function AutoFish.Aktif()
     while true do
         pcall(function()
-          
+            Events.equip:FireServer(1)
+            task.wait(0.05)
+            Events.charge:InvokeServer(1755848498.4834)
+            task.wait(0.02)
+            Events.minigame:InvokeServer(1.2854545116425, 1)
         end)
         wait(1)
     end
