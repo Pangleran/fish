f46a81ccc5ec6bf796ff3ff46bc6cbd04ca096b7
Pangleran <mukhammadresky24@gmail.com>
2025-11-25 03:49:34 +0000

    auto commit Tue Nov 25 03:49:34 AM UTC 2025

--- autofish.lua
+++ autofish.lua
@@ -1,5 +1,18 @@
 local AutoFish = {}
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local net = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net
+
+local Events = {
+    fishing = net:WaitForChild("RE/FishingCompleted"),
+    charge = net:WaitForChild("RF/ChargeFishingRod"),
+    minigame = net:WaitForChild("RF/RequestFishingMinigameStarted"),
+    cancel = net:WaitForChild("RF/CancelFishingInputs"),
+    equip = net:WaitForChild("RE/EquipToolFromHotbar"),
+    unequip = net:WaitForChild("RE/UnequipToolFromHotbar"),
+}
+
 function AutoFish.Aktif()
     pcall(function()
         -- code
