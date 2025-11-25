5d91e14b46832ef031c615e494f64db69b905fd0
Pangleran <mukhammadresky24@gmail.com>
2025-11-25 05:36:21 +0000

    auto commit Tue Nov 25 05:36:21 AM UTC 2025

--- autofish.lua
+++ autofish.lua
@@ -12,40 +12,39 @@ local Events = {
     equip = net:WaitForChild("RE/EquipToolFromHotbar"),
     unequip = net:WaitForChild("RE/UnequipToolFromHotbar"),
 }
-        
+
 
 AutoFish.Running = false
 
 function AutoFish.Aktif()
-AutoFish.Running = true
+    AutoFish.Running = true
 
-while AutoFish.Running do
-    pcall(function()
-        Events.equip:FireServer(1)
-        task.wait(0.02)
+    while AutoFish.Running do
+        pcall(function()
+            Events.equip:FireServer(1)
+            task.wait(0.02)
 
-        Events.charge:InvokeServer(1755848498.4834)
-        task.wait(0.01)
+            Events.charge:InvokeServer(1755848498.4834)
+            task.wait(0.01)
 
-        Events.minigame:InvokeServer(1.2854545116425, 1)
-        task.wait(0.25)
+            Events.minigame:InvokeServer(1.2854545116425, 1)
+            task.wait(0.25)
 
-        for i = 1, 5 do
-            Events.fishing:FireServer()
-            task.wait(0.005)
-        end
+            for i = 1, 5 do
+                Events.fishing:FireServer()
+                task.wait(0.005)
+            end
 
-        task.wait(0.05)
-    end)
-end
+            task.wait(0.05)
+        end)
+    end
 end
 
 function AutoFish.Nonaktif()
-AutoFish.Running = false
-pcall(function()
-    Events.unequip:FireServer()
-end)
+    AutoFish.Running = false
+    pcall(function()
+        Events.unequip:FireServer()
+    end)
 end
-
 
 return AutoFish
