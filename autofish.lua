6934b296101d5d84ff5fc3bfecbca36e1db73f51
Pangleran <mukhammadresky24@gmail.com>
2025-11-25 03:30:03 +0000

    auto commit Tue Nov 25 03:30:03 AM UTC 2025

--- autofish.lua
+++ autofish.lua
@@ -6,6 +6,15 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local LocalPlayer = Players.LocalPlayer
 
 function AutoFish.Aktif()
-    
+    pcall(function()
+        if Events.equip and Events.charge and Events.minigame then
+            Events.equip:FireServer(1)
+            task.wait(0.05)
+            Events.charge:InvokeServer(1755848498.4834)
+            task.wait(0.02)
+            Events.minigame:InvokeServer(1.2854545116425, 1)
+        end
+    end)
+end
 
 return AutoFish
