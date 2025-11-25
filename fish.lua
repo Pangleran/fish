f79edeff31de2a3c52c228f22c5e2fe1aa71ba75
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-25 01:07:09 +0000

    auto commit Tue Nov 25 01:07:09 AM UTC 2025

--- fish.lua
+++ fish.lua
@@ -5,7 +5,7 @@ local LowTexture = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/lowtexture.lua"))()
 local AntiAfk = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/antiafk.lua"))()
 local AutoFish = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/autofish.lua"))()
-local SwitchServer = loadString
+local SwitchServer = loadstring(game:HttpGet(""))
 
 local Window = Rayfield:CreateWindow({
     Name = "Fish it - Iky Fareza",
