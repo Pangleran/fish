28965bcef7e175a9d4a4c87a1e21f4a8f70c093d
Pangleran <mukhammadresky24@gmail.com>
2025-11-25 03:03:14 +0000

    auto commit Tue Nov 25 03:03:14 AM UTC 2025

--- lowtexture.lua
+++ lowtexture.lua
@@ -2,25 +2,50 @@ local LowTexture = {}
 
 function LowTexture.Aktif()
     pcall(function()
+        for _, v in pairs(game:GetDescendants()) do
+            if v:IsA("BasePart") then
+                v.Material = Enum.Material.SmoothPlastic
+                v.Reflectance = 0
+            elseif v:IsA("Decal") or v:IsA("Texture") then
+                v.Transparency = 1
+            end
+        end
+
+        local Lighting = game:GetService("Lighting")
+        for _, effect in pairs(Lighting:GetChildren()) do
+            if effect:IsA("PostEffect") then
+                effect.Enabled = false
+            end
+        end
+
+        Lighting.GlobalShadows = false
+        Lighting.FogEnd = 1e10
         settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
-        game.Lighting.GlobalShadows = false
-        game.Lighting.FogEnd = 1
-        setfpscap(8)
     end)
 end
 
-
--- ======================
--- FUNGSI NONAKTIF
--- ======================
 function LowTexture.Nonaktif()
     pcall(function()
-        settings().Rendering.QualityLevel = Enum.QualityLevel.Level02
-        game.Lighting.GlobalShadows = true
-        game.Lighting.FogEnd = 100000
-        setfpscap(0)
+        for _, v in pairs(game:GetDescendants()) do
+            if v:IsA("BasePart") then
+                v.Material = Enum.Material.Plastic
+                v.Reflectance = 0
+            elseif v:IsA("Decal") or v:IsA("Texture") then
+                v.Transparency = 0
+            end
+        end
+
+        local Lighting = game:GetService("Lighting")
+        for _, effect in pairs(Lighting:GetChildren()) do
+            if effect:IsA("PostEffect") then
+                effect.Enabled = true
+            end
+        end
+
+        Lighting.GlobalShadows = true
+        Lighting.FogEnd = 100000
+        settings().Rendering.QualityLevel = Enum.QualityLevel.Level10
     end)
 end
-
 
 return LowTexture
