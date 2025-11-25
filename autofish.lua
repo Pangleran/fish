8fc53a5f399cac3c75d86ba0675c2b7c23bf9be3
Pangleran <mukhammadresky24@gmail.com>
2025-11-25 03:39:28 +0000

    auto commit Tue Nov 25 03:39:28 AM UTC 2025

--- autofish.lua
+++ autofish.lua
@@ -6,20 +6,4 @@ function AutoFish.Aktif()
     end)
 end
 
---
-
--- CAST / MELEMPAR
--- pcall(function()
---     Events.equip:FireServer(1)
---     task.wait(0.05)
---     Events.charge:InvokeServer(1755848498.4834)
---     task.wait(0.02)
---     Events.minigame:InvokeServer(1.2854545116425, 1)
--- end)
-
--- REEL / MENARIK
--- pcall(function()
---     Events.fishing:FireServer()
--- end)
-
 return AutoFish
