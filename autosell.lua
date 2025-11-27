d65e236348060c8509ca8a64bad654744baed5ff
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-27 09:15:41 +0800

    Update autosell.lua

--- autosell.lua
+++ autosell.lua
@@ -1,1 +1,25 @@
+local AutoSell = {}
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local net = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net
+
+local sell = net:WaitForChild("RF/SellAllItems")
+local LoopSell = false
+
+function AutoSell.run()
+  LoopSell = true
+  task.wait(1)
+  
+  while LoopSell do
+    pcall(function()
+        sell:InvokeServer()
+      end)
+    task.wait(1 * 60)
+  end
+end
+
+function AutoSell.stop()
+  LoopSell = false
+end
+
+return AutoSell
