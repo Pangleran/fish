fedf6db6e6544c489777cfdbbcecb2c8d86fc7da
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-28 15:42:32 +0800

    Update fish.lua

--- fish.lua
+++ fish.lua
@@ -1,5 +1,7 @@
 local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua", true))()
 
+Luna.Configuration.InterfaceToggleNotifications = false
+
 local TeleportModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/teleport.lua"))()
 local LowTexture = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/lowtexture.lua"))()
 local AntiAfk = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/antiafk.lua"))()
