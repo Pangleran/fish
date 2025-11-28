7ebc5e536728b2daa71670ed897433b8a6d80337
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-28 15:24:34 +0800

    Update autofish.lua

--- autofish.lua
+++ autofish.lua
@@ -13,20 +13,10 @@ local Events = {
 }
 
 AutoFish.Running = false
-AutoFish.DelayCastValue = 1
-AutoFish.DelayReelValue = 5
-AutoFish.DelayCompleteValue = 0.2
+AutoFish.DelayFishing = 1
 
-function AutoFish.SetDelayCast(v)
-    AutoFish.DelayCastValue = v
-end
-
-function AutoFish.SetDelayReel(v)
-    AutoFish.DelayReelValue = v
-end
-
-function AutoFish.SetDelayComplete(v)
-    AutoFish.DelayCompleteValue = v
+function AutoFish.SetDelayFishing(v)
+    AutoFish.DelayFishing = v
 end
 
 function AutoFish.run()
@@ -43,14 +33,14 @@ function AutoFish.run()
             Events.minigame:InvokeServer(1.2854545116425, 1)
         end)
 
-        task.wait(AutoFish.DelayCastValue)
+        task.wait(AutoFish.DelayFishing)
 
-        for i = 1, AutoFish.DelayReelValue do
+        for i = 1, 3 do
             Events.fishing:FireServer()
             task.wait(0.01)
         end
 
-        task.wait(AutoFish.DelayCompleteValue)
+        task.wait(0.1)
     end
 end
 
