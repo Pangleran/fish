024d32b6ae0d79da70b8302c9a83510d209c865a
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-28 21:37:25 +0800

    Update fish.lua

--- fish.lua
+++ fish.lua
@@ -1,5 +1,24 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+WindUI:Gradient({
+    ["0"] = {
+        Color = Color3.fromHex("#00111d"), -- Biru sangat gelap (deep ocean)
+        Transparency = 0
+    },
+
+    ["50"] = {
+        Color = Color3.fromHex("#004e89"), -- Biru tua ke laut dalam
+        Transparency = 0
+    },
+
+    ["100"] = {
+        Color = Color3.fromHex("#009dff"), -- Biru muda terang seperti permukaan air
+        Transparency = 0
+    },
+}, {
+    Rotation = 90, -- Bisa 0, 45, 90, 180 sesuai arah gradient
+})
+
 local TeleportModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/teleport.lua"))()
 local LowTexture = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/lowtexture.lua"))()
 local AntiAfk = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pangleran/rblx/refs/heads/main/antiafk.lua"))()
